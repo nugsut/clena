4525f67d3cb426f349227fcf7788bff0ff9b279a
nugsut <135144961+nugsut@users.noreply.github.com>
2024-09-17 21:23:52 +0300

    Add files via upload

--- Check%20(1).bat
+++ Check%20(1).bat
@@ -1,30 +1,48 @@
 @echo off
 mode con: cols=90 lines=48
-:diocane
-title REEEEEEEEEEEEEEEEEEEEEEEEE
-color 0c
-cls
+setlocal enabledelayedexpansion
 
-echo BIOS
-echo [92m------------[91m
-wmic bios get serialnumber
+:: Define the output file path
+set "outputFile=%USERPROFILE%\Downloads\SystemInfo.txt"
 
-echo SMBIOS UUID
-echo [92m------------[91m
-wmic csproduct get uuid
+:: Clear previous output file if it exists
+if exist "%outputFile%" del "%outputFile%"
 
-echo CPU
-echo [92m------------[91m
-wmic cpu get serialnumber
-
-echo Baseboard
-echo [92m------------[91m
-wmic baseboard get serialnumber
-
-echo MAC Address
-echo [92m------------[91m
-wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress
-
-echo Press ENTER to check information again...
+:: Collect and output BIOS serial number
+echo BIOS >> "%outputFile%"
+echo ------------ >> "%outputFile%"
+wmic bios get serialnumber >> "%outputFile%" 2>&1
+echo BIOS information collected. Press any key to continue...
 pause >nul
-goto diocane
+
+:: Collect and output SMBIOS UUID
+echo SMBIOS UUID >> "%outputFile%"
+echo ------------ >> "%outputFile%"
+wmic csproduct get uuid >> "%outputFile%" 2>&1
+echo SMBIOS UUID information collected. Press any key to continue...
+pause >nul
+
+:: Collect and output CPU serial number
+echo CPU >> "%outputFile%"
+echo ------------ >> "%outputFile%"
+wmic cpu get serialnumber >> "%outputFile%" 2>&1
+echo CPU information collected. Press any key to continue...
+pause >nul
+
+:: Collect and output Baseboard serial number
+echo Baseboard >> "%outputFile%"
+echo ------------ >> "%outputFile%"
+wmic baseboard get serialnumber >> "%outputFile%" 2>&1
+echo Baseboard information collected. Press any key to continue...
+pause >nul
+
+:: Collect and output MAC Address
+echo MAC Address >> "%outputFile%"
+echo ------------ >> "%outputFile%"
+wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress >> "%outputFile%" 2>&1
+echo MAC Address information collected. Press any key to continue...
+pause >nul
+
+echo All information has been collected. Press any key to exit...
+pause >nul
+	
